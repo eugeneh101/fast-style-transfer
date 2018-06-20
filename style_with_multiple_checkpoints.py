970fb1d1de64d14a7b9e64f611e5ebc4afd7cbe6
Ubuntu <ubuntu@ip-172-31-21-94.ec2.internal>
2018-06-20 15:11:21 +0000

    currently a work in progress

--- style_with_multiple_checkpoints.py
+++ style_with_multiple_checkpoints.py
@@ -6,6 +6,9 @@ from optimize_with_multiple_checkpoints import optimize
 from argparse import ArgumentParser
 from utils import save_img, get_img, exists, list_files
 import evaluate
+import time
+from datetime import datetime
+
 
 CONTENT_WEIGHT = 7.5e0
 STYLE_WEIGHT = 1e2
@@ -89,6 +92,16 @@ def build_parser():
                         dest='device_and_number',
                         help='device and its associated GPU number or CPU count',
                         metavar='DEVICE', required=True)
+    
+    parser.add_argument('--max-runtime-in-minutes', type=int,
+                        dest='max_runtime_in_minutes',
+                        help='maximum runtime in minutes before automatic shut down',
+                        metavar='MAX_RUNTIME_IN_MINUTES', default=None)
+
+    parser.add_argument('--log_file', type=str,
+                        dest='log_file',
+                        help='file you want to write the print statements to',
+                        metavar='LOG_FILE', default=None)
 
     return parser
 
@@ -108,6 +121,9 @@ def check_opts(opts):
     assert opts.style_weight >= 0
     assert opts.tv_weight >= 0
     assert opts.learning_rate >= 0
+    assert opts.max_runtime_in_minutes >= 0
+    if opts.log_file:
+        assert os.path.isdir(os.path.dirname(opts.log_file))
 
 def _get_files(img_dir):
     files = list_files(img_dir)
@@ -117,8 +133,11 @@ def _get_files(img_dir):
 def main():
     parser = build_parser()
     options = parser.parse_args()
-    check_opts(options)
+    _run_model(options)
 
+
+def _run_model(options):
+    check_opts(options)
     style_target = get_img(options.style)
     if not options.slow:
         content_targets = _get_files(options.train_path)
@@ -150,9 +169,12 @@ def main():
         options.vgg_path
     ]
 
-    import time
-    from datetime import datetime
     start_time = time.time()
+    shutdown_time = (start_time +  options.max_runtime_in_minutes * 60 
+                     if options.max_runtime_in_minutes else float('inf'))
+    if options.log_file:
+        log_file_ = open(options.log_file, 'w')
+        sys.stdout = log_file_
     for preds, losses, i, epoch, checkpoint_number in optimize(*args, **kwargs):
         style_loss, content_loss, tv_loss, loss = losses
         delta_time, start_time = time.time() - start_time, time.time()        
@@ -170,9 +192,61 @@ def main():
                 evaluate.ffwd_to_img(options.test, preds_path, actual_dir)
             else:
                 save_img(preds_path, img)
+        if time.time() > shutdown_time: # automatic shut down
+            print('Ran for maximum runtime in minutes. Now shutting down time.')
+            break
     ckpt_dir = options.checkpoint_dir
     cmd_text = 'python evaluate.py --checkpoint %s ...' % ckpt_dir
     print("Training complete. For evaluation:\n    `%s`" % cmd_text)
+    if options.log_file:
+        log_file_.close()
+    
 
+class ArgumentObject(object):
+    def __init__(self, style, checkpoint_dir, device_and_number, test, test_dir, 
+                 epochs, checkpoint_iterations, batch_size, max_runtime_in_minutes,
+                 log_file, train_path, vgg_path, content_weight, style_weight, 
+                 tv_weight, learning_rate, slow):
+        self.style = style
+        self.checkpoint_dir = checkpoint_dir
+        self.device_and_number = device_and_number
+        self.test = test
+        self.test_dir = test_dir
+        self.epochs = epochs
+        self.checkpoint_iterations = checkpoint_iterations
+        self.batch_size = batch_size
+        self.max_runtime_in_minutes = max_runtime_in_minutes
+        self.log_file = log_file
+        self.train_path = train_path
+        self.vgg_path = vgg_path
+        self.content_weight = content_weight
+        self.style_weight = style_weight
+        self.tv_weight = tv_weight
+        self.learning_rate = learning_rate
+        self.slow = slow    
+        
+def create_checkpoints(
+        # mandatory arguments
+        style, checkpoint_dir, device_and_number,
+
+        # useful argments
+        test=False, test_dir=False, epochs=NUM_EPOCHS, 
+        checkpoint_iterations=CHECKPOINT_ITERATIONS, batch_size=BATCH_SIZE,
+        max_runtime_in_minutes=None,
+        log_file=None,
+
+        # not useful arguments you probably shouldn't touch
+        train_path=TRAIN_PATH, vgg_path=VGG_PATH, content_weight=CONTENT_WEIGHT, 
+        style_weight=STYLE_WEIGHT, tv_weight=TV_WEIGHT, learning_rate=LEARNING_RATE, slow=False
+):
+    options = ArgumentObject(style, checkpoint_dir, device_and_number, test, 
+            test_dir, epochs, checkpoint_iterations, batch_size, 
+             max_runtime_in_minutes, log_file, train_path, vgg_path, 
+             content_weight, style_weight, tv_weight, learning_rate, slow)
+    _run_model(options)
+    
+    
+    
+    
 if __name__ == '__main__':
     main()
